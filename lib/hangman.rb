d9f99ff4eaf12f0cb797337c03c3a32ca7c31a37
Prabhjot <pjsingh1359@gmail.com>
2021-03-18 15:43:10 -0700

    working on loading function

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,21 +4,21 @@ require_relative 'display'
 class Hangman
   include Display
   attr_reader :word
-  attr_accessor :arr
+  attr_accessor :arr, :mistakes, :incorrect_letters
 
   def initialize
     @word = pick_random_line.downcase
     @arr = word_grid(word)
+    @incorrect_letters = []
+    @mistakes = 0
   end
 
   def play_game
     puts word  # remove this when finished
-    incorrect_letters = []
-    mistakes = 0
     intro(arr, incorrect_letters, mistakes)
     until mistakes == 6
       input = gets.chomp
-      input_result(input, word, arr, incorrect_letters, mistakes)
+      input_result(input)
       score(arr, incorrect_letters, mistakes)
       break unless game_over?(arr)
     end
@@ -34,34 +34,34 @@ class Hangman
     end
   end
 
-  def check_word(input, word, arr, wrong_words, mistakes)
+  def check_word(input)
     word_split = word.split('')
     word_split.each_with_index do |val, idx|
       if val == input && arr[idx] == '_'
         arr[idx] = input
       elsif !word.include?(input) || (val == input && arr[idx] != '_')
-        wrong_words.push(input)
-        mistakes += 1
+        incorrect_letters.push(input)
+        @mistakes += 1
         return false
       end
     end
-    true
   end
 
   def word_grid(word)
     Array.new(word.length - 1, '_')
   end
 
-  def input_result(input, word, arr, incorrect_letters, mistakes)
+  def input_result(input)
     if input == '1'
       save_game
     elsif input == '2'
       load_game
+      output
     elsif input.downcase.match(/[a-z]/) && input.length == 1
-      check_word(input, word, arr, incorrect_letters, mistakes)
+      check_word(input)
     elsif input.empty? || input.length > 1
       puts 'Please enter one letter'
-      input_result(gets.chomp, word, arr, incorrect_letters, mistakes)
+      input_result(gets.chomp)
     end
   end
 
@@ -75,23 +75,34 @@ class Hangman
   end
 
   def save_game
-    CSV.open('saved_game.csv', 'w', write_headers: true, headers: ['word', 'array']) do |hdr|
-      data_out = [word, arr]
+    CSV.open('saved_game.csv', 'w', write_headers: true, headers: ['word', 'array', 'mistakes', 'incorrect_letters']) do |hdr|
+      data_out = [word, arr, mistakes, incorrect_letters]
       hdr << data_out
     end
     puts 'Game Saved!'
   end
 
   def load_game
-    content = CSV.open('saved_game.csv', headers: true, header_converters: :symbol)
-
-    content.each do |row|
-      word = row[:word]
-      array = row[:array]
-
-      puts word
-      puts array
+    CSV.foreach('saved_game.csv', headers: true, header_converters: :symbol) do |row|
+      @word = row[:word]
+      @arr = row[:array]
+      @incorrect_letters = row[:incorrect_letters]
+      @mistakes = row[:mistakes]
     end
+
+    # content.each do |row|
+      # @word = row[:word]
+      # @arr = row[:array]
+      # @incorrect_letters = row[:incorrect_letters]
+      # @mistakes = row[:mistakes]
+    # end
+    # puts @incorrect_letters
+    puts 'Save Loaded!'
+  end
+
+  def output 
+    puts incorrect_letters
+    puts arr
   end
 end
 
