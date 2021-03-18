845d3f051bd0af36c90b0ca4d7358ece9b1c6066
Prabhjot <pjsingh1359@gmail.com>
2021-03-18 07:13:14 -0700

    converted to class

--- lib/display.rb
+++ lib/display.rb
@@ -1,18 +1,19 @@
-def intro(arr, incorrect_letters, mistakes)
-  puts ''
-  puts 'welcome to hangman! In this game you the player will have have 6 tries to guess the correct word.'
-  puts "Failure to do so will result in getting hanged (ouch!). Goodluck!\n\n"
-  puts "Press '1' so save your game"
-  puts "Press '2' to load your save\n\n"
+module Display
+  def intro(arr, incorrect_letters, mistakes)
+    puts ''
+    puts 'welcome to hangman! In this game you the player will have have 6 tries to guess the correct word.'
+    puts "Failure to do so will result in getting hanged (ouch!). Goodluck!\n\n"
+    puts "Press '1' so save your game"
+    puts "Press '2' to load your save\n\n"
 
-  puts "You are looking for a #{arr.length} letter word!\n\n"
+    puts "You are looking for a #{arr.length} letter word!\n\n"
 
-  score(arr, incorrect_letters, mistakes)
+    score(arr, incorrect_letters, mistakes)
+  end
+
+  def score(arr, incorrect_letters, mistakes = nil)
+    puts "mistakes: #{mistakes} of 6"
+    puts "Correct words: #{arr}"
+    puts "Incorrect words: #{incorrect_letters}"
+  end
 end
-
-def score(arr, incorrect_letters, mistakes = nil)
-  puts "mistakes: #{mistakes} of 6"
-  puts "Correct words: #{arr}"
-  puts "Incorrect words: #{incorrect_letters}"
-end
-
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,69 +1,73 @@
 require_relative 'display'
 
-def pick_random_line
-  word = File.readlines('word_bank.txt').sample
-  if (word.length - 1) < 5 || (word.length - 1) > 12
-    pick_random_line
-  else
-    word
+class Hangman
+  include Display
+  attr_reader :word
+  attr_accessor :arr
+  def initialize
+    @word = pick_random_line.downcase
+    @arr = word_grid(word)
   end
-end
 
-def word_grid(word)
-  Array.new(word.length - 1, '_')
-end
-
-def play_game(word)
-  puts word  # remove this when finished
-  arr = word_grid(word)
-  incorrect_letters = []
-  mistakes = 0
-  intro(arr, incorrect_letters, mistakes)
-  until mistakes == 6
-    input = correct_input(gets.chomp)
-    mistakes += 1 unless check_word(input, word, arr, incorrect_letters)
-    score(arr, incorrect_letters, mistakes)
-    break unless game_over?(arr)
-  end
-end
-
-def game_over?(arr)
-  if arr.include?('_')
-    true
-  else
-    puts 'Game Over!'
-  end
-end
-
-def check_word(input, word, arr, wrong_words)
-  word_split = word.split('')
-  word_split.each_with_index do |val, idx|
-    if val == input && arr[idx] == '_'
-      arr[idx] = input
-    elsif !word.include?(input) || (val == input && arr[idx] != '_')
-      wrong_words.push(input)
-      return false
+  def play_game
+    puts word  # remove this when finished
+    incorrect_letters = []
+    mistakes = 0
+    intro(arr, incorrect_letters, mistakes)
+    until mistakes == 6
+      input = correct_input(gets.chomp)
+      mistakes += 1 unless check_word(input, word, arr, incorrect_letters)
+      score(arr, incorrect_letters, mistakes)
+      break unless game_over?(arr)
     end
   end
-  true
-end
 
-def save_game(word, arr)
+  private
 
-end
+  def game_over?(arr)
+    if arr.include?('_')
+      true
+    else
+      puts 'Game Over!'
+    end
+  end
 
-def load_game
-  
-end
+  def check_word(input, word, arr, wrong_words)
+    word_split = word.split('')
+    word_split.each_with_index do |val, idx|
+      if val == input && arr[idx] == '_'
+        arr[idx] = input
+      elsif !word.include?(input) || (val == input && arr[idx] != '_')
+        wrong_words.push(input)
+        return false
+      end
+    end
+    true
+  end
 
-def correct_input(input)
-  if input.downcase.match(/[a-z]/) && input.length == 1
-    input
-  elsif input.empty? || input.length > 1
-    puts 'Please enter one letter'
-    correct_input(gets.chomp)
+  def word_grid(word)
+    Array.new(word.length - 1, '_')
+  end
+
+  def correct_input(input)
+    if input.downcase.match(/[a-z]/) && input.length == 1
+      input
+    elsif input.empty? || input.length > 1
+      puts 'Please enter one letter'
+      correct_input(gets.chomp)
+    end
+  end
+
+  def pick_random_line
+    word = File.readlines('word_bank.txt').sample
+    if (word.length - 1) < 5 || (word.length - 1) > 12
+      pick_random_line
+    else
+      word
+    end
   end
 end
 
-play_game(pick_random_line)
+game = Hangman.new
 
+game.play_game
